f25d2397c13028e782a9e985071d0ea4592c0de6
HappySK <shravanyoserene1729@gmail.com>
2021-05-31 16:52:03 +0530

    Updating baseURL fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API = axios.create({ baseURL: "http://localhost:5000" });
+export const API = axios.create({ baseURL: "https://ancient-gorge-85187.herokuapp.com" });
 
 export const signup = async (user) => await API.post("/user/signup", user);
 export const signin = async (user) => await API.post("/user/signin", user);
